38326768a22e724c6c387ab1e606d8b62b9d6913
Pranav Aditya Singh <93078167+NAVadi21@users.noreply.github.com>
2023-05-20 13:41:52 +0530

    Update 1048-longest-string-chain.java with a new implementation using HashMaps

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,19 +1,53 @@
-class Solution {
-    public int longestStrChain(String[] words) {
-        Map<String,Integer> map = new HashMap<>();
-        Arrays.sort(words, (a,b)->a.length() - b.length());
-        int res = 0;
-        for(String word : words)
-        {
-            int best = 0;
-            for(int i=0;i<word.length();i++)
-            {
-                String prev = word.substring(0,i) + word.substring(i+1);
-                best = Math.max(best,map.getOrDefault(prev,0)+1);
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class WordChain {
+
+    public static int longestWordChain(String[] words) {
+        // Sort the words array by length
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+
+        // Store the maximum length of word chains ending at each word
+        Map<String, Integer> wordChainLengths = new HashMap<>();
+
+        int maxLength = 0;
+        for (String word : words) {
+            int currentLength = 0;
+
+            // Find all possible predecessors of the current word and update the maximum length
+            for (int i = 0; i < word.length(); i++) {
+                String predecessor = word.substring(0, i) + word.substring(i + 1);
+
+                // Update the current length if a predecessor exists and has a longer chain
+                currentLength = Math.max(currentLength, wordChainLengths.getOrDefault(predecessor, 0) + 1);
             }
-            map.put(word,best);
-            res = Math.max(res,best);
+
+            // Store the maximum length of the chain ending at the current word
+            wordChainLengths.put(word, currentLength);
+
+            // Update the overall maximum length
+            maxLength = Math.max(maxLength, currentLength);
         }
-        return res;
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of words: ");
+        int n = scanner.nextInt();
+
+        String[] words = new String[n];
+        scanner.nextLine(); // Consume the newline character
+
+        System.out.println("Enter the words:");
+        for (int i = 0; i < n; i++) {
+            words[i] = scanner.nextLine();
+        }
+
+        int longestChainLength = longestWordChain(words);
+        System.out.println("Longest Word Chain Length: " + longestChainLength);
     }
 }
