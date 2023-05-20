ecc52f6da8cef0f93f6efc897141ed52f787b797
Pranav Aditya Singh <93078167+NAVadi21@users.noreply.github.com>
2023-05-20 13:49:51 +0530

    Update 1480-running-sum-of-1d-array.java with a different implementation than the previous one to get the running sum from the input

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,7 +1,34 @@
-class Solution {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RunningSum {
     public int[] runningSum(int[] nums) {
-        for(int i=1;i<nums.length;i++)
-           nums[i]+=nums[i-1];
-        return nums;
+        int n = nums.length;
+        int[] runningSum = new int[n];
+
+        runningSum[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            runningSum[i] = runningSum[i - 1] + nums[i];
+        }
+
+        return runningSum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        RunningSum runningSum = new RunningSum();
+        int[] result = runningSum.runningSum(nums);
+        System.out.println("Running Sum: " + Arrays.toString(result));
     }
 }
